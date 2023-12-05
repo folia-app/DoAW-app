42381539b19ec2f5916844971bf2df674b1c9209
evvvritt <evv.info@gmail.com>
2023-12-05 14:21:02 +0100

    joan info edits

--- src/components/InfoModal.vue
+++ src/components/InfoModal.vue
@@ -2,7 +2,7 @@
 <Modal maxWidth="51em" class="text-center">
   <header class="mt-2">
     <h1>DoAW</h1>
-    (dead or alive wallet)<br><br>
+    Dead or Alive Wallet<br><br>
     by JOAN HEEMSKERK<br><br>
     ~ presented by <a href="https://folia.app" class="underline" target="_blank">folia</a> ~<br>
   </header>
@@ -10,7 +10,7 @@
     <li>click START to start the engine</li>
     <li>PAUSE the engine on your favorite <span v-for="(char, i) in ('SEEDPHRASE').split('')" :key="i" :style="{color: stringToHexColor(Array(6).fill(char).join())}">{{ char }}</span></li>
     <li>CONNECT your ethereum wallet</li>
-    <li>MINT a DoAW (and win)</li>
+    <li>MINT a DoAW</li>
   </ol>
   <footer class="flex justify-evenly mb-2">
     <a :href="$store.getters.etherscanLink({})" class="underline" target="_blank" rel="noopener noreferrer">CONTRACT</a>
