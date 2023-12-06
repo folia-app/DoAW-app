285071fd689b6330d2463de695ae7baecc668fe8
evvvritt <evv.info@gmail.com>
2023-12-06 14:47:10 +0100

    mint randoms ui

--- src/components/MintModal.vue
+++ src/components/MintModal.vue
@@ -1,13 +1,16 @@
 <template>
 <Modal maxWidth="30em">
   <div class="flex flex-col gap-6 py-10">
-    <h6>MINT DoAW:</h6>
+    <h6 class="pl-2">PHRASE:</h6>
     <p class="leading-[2] uppercase">
       <span v-for="word in words.split(' ')" :key="word" class="inline-block pr-3" :style="{'color': stringToHexColor(word)}">
         {{ word }}
       </span>
     </p>
-    <div>?</div>
+    <!-- <div>?</div> -->
+    <div class="flex justify-center">
+      <button class="p-2 -my-2"><SvgRefresh class="h-[1.3em]" /></button>
+    </div>
   </div>
   <div class="flex flex-col gap-3">
     <!-- price -->
@@ -20,9 +23,17 @@
     </div>
     <ConnectButton connectedTheme="bg-neutral-900 text-white" />
     <button class="flex h-12 items-center justify-center border" :class="{'border-dotted opacity-50 cursor-not-allowed': !canMint}" :disabled="!canMint" @click="mint(props.entropyHex)">
-      MINT
+      MINT PHRASE
     </button>
-    
+    <div class="flex border h-12" :class="{'border-dotted opacity-50 cursor-not-allowed': !canMint}">
+      <button class="flex-1 flex items-center justify-center" :disabled="!canMint" @click="mint(props.entropyHex)">
+        MINT RANDOM
+      </button>
+      <div class="w-14 flex flex-col border-l" :class="{'border-dotted': !canMint}">
+        <button class="flex-1 flex items-center justify-center pl-1" :class="{'bg-white text-black': qty === 1}" @click="qty = 1">1x</button>
+        <button class="flex-1 flex items-center justify-center pl-1" :class="{'bg-white text-black': qty === 3}" @click="qty = 3">3x</button>
+      </div>
+    </div>
     <!-- (mint status) -->
     <div v-if="status" class="py-1 text-black" :class="{'bg-red-300': status.type === 'error', 'bg-green-300': status.type === 'success', 'bg-grau-200': !status.type, 'animate-blink-slow': status.message.includes('...')}">
       {{ status.message }}
@@ -43,6 +54,7 @@ import hexToBytes from '../utils/hexToBytes';
 import stringToHexColor from '../utils/stringToHexColor';
 import Modal from './Modal.vue';
 import { useRoute, useRouter } from 'vue-router';
+import SvgRefresh from '../components/SvgRefresh.vue'
 
 const props = defineProps(['entropyHex'])
 const emit = defineEmits('close')
@@ -53,6 +65,7 @@ const data = computed(() => props.entropyHex && hexToBytes(props.entropyHex))
 const words = computed(() => data.value && utils.entropyToMnemonic(data.value))
 
 // minting
+const qty = ref(1)
 const status = ref()
 const txs = computed(() => store.state.pending.filter(tx => tx.name === 'mint'))
 async function mint(entropy) {
---
+++ src/components/SvgRefresh.vue
@@ -0,0 +1,5 @@
+<template>
+<svg viewBox="0 0 100 115" fill="none" xmlns="http://www.w3.org/2000/svg" preserve-aspect-ratio>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M80 0H70V10H80V20H10V30H0V55H10V30H80V40H70V50H80V40H90V30H100V20H90V10H80V0ZM0 95H10V105H20V115H30V105H20V95H90V85H100V60H90V85H20V75H30V65H20V75H10V85H0V95Z" fill="currentColor"/>
+</svg>
+</template>
