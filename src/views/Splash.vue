fd2427ff191801f62c6632e8c1176bad5e1ef72c
evvvritt <evv.info@gmail.com>
2023-12-07 15:35:00 +0100

    reveal bar

--- src/views/Splash.vue
+++ src/views/Splash.vue
@@ -2,7 +2,7 @@
   <article class="fixed overlay flex flex-col">
     <iframe ref="iframeEl" :src="iframeUrl" class="flex-1 w-full" />
 
-    <!-- <div class="pt-1.5 sm:pt-2 pb-2 sm:flex flex-wrap items-center text-center sm:text-left justify-between px-2.5" style="font-size:0.875em">
+    <div class="pt-1.5 sm:pt-2 pb-2 sm:flex flex-wrap items-center text-center sm:text-left justify-between px-2.5" style="font-size:0.875em">
       <div class="max-w-full">JOAN HEEMSKERK</div>
       <div class="pr-0.5">DEAD OR ALIVE WALLET</div>
       <div class="relative">
@@ -11,7 +11,7 @@
           <a href="https://folia.app/works/joan-heemskerk-doaw" target="_blank" class="underline">12.12.2023</a>
         </div>
       </div>
-    </div> -->
+    </div>
   </article>
 </template>
 
