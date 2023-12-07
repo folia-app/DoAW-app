49b98ac59717b705c8f682b8603d9474c48fa6bf
evvvritt <evv.info@gmail.com>
2023-12-07 14:54:15 +0100

    splash updates

--- src/views/Splash.vue
+++ src/views/Splash.vue
@@ -2,9 +2,15 @@
   <article class="fixed overlay flex flex-col">
     <iframe ref="iframeEl" :src="iframeUrl" class="flex-1 w-full" />
 
-    <div class="py-2 flex flex-wrap items-center justify-between px-2.5">
-      <div class="max-w-full">JOAN HEEMSKERK: DoAW</div>
-      <div><a href="https://folia.app/works/joan-heemskerk-doaw" target="_blank" class="underline">12.12.2023</a></div>
+    <div class="pt-1.5 sm:pt-2 pb-2 sm:flex flex-wrap items-center text-center sm:text-left justify-between px-2.5" style="font-size:0.875em">
+      <div class="max-w-full">JOAN HEEMSKERK</div>
+      <div class="pr-0.5">DEAD OR ALIVE WALLET</div>
+      <div class="relative">
+        <div class="invisible">JOAN HEEMSKERK</div>
+        <div class="absolute overlay text-center sm:text-right">
+          <a href="https://folia.app/works/joan-heemskerk-doaw" target="_blank" class="underline">12.12.2023</a>
+        </div>
+      </div>
     </div>
   </article>
 </template>
