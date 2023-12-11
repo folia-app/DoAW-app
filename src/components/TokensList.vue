f3346c5f425f371a424a2d603ace002bb514968e
evvvritt <evv.info@gmail.com>
2023-12-12 00:17:03 +0100

    list, remove 000

--- src/components/TokensList.vue
+++ src/components/TokensList.vue
@@ -13,8 +13,8 @@
         <li v-for="(token, index) in renderedTokens" :key="token.tokenId + '_' + index" class="flex flex-wrap md:flex-nowrap min-h-9 justify-between">
           <!-- token link -->
           <div v-if="demoAmount">{{ index }}</div>
-          <router-link :to="'/tokens/' + token.tokenId" class="order-1 px-2.5 py-2.5">
-            <div>#{{ ('0000' + (token.index)).slice(-4) }}</div>
+          <router-link :to="'/tokens/' + token.tokenId" class="order-1 pl-2.5 pr-1.5 py-2.5 block">
+            #{{ token.index }}
           </router-link>
 
           <router-link :to="'/tokens/' + token.tokenId" class="order-3 px-2.5 pb-2.5 md:py-2.5 md:flex-1 text-left mouse:hover:bg-[rgba(255,255,255,0.1)]">
