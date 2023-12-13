d880b7af8a7f20de9cb12d38b7cbeb87e47be435
evvvritt <evv.info@gmail.com>
2023-12-13 20:44:37 +0100

    shadoaws dim if account0

--- src/views/IndexShadoaws.vue
+++ src/views/IndexShadoaws.vue
@@ -13,33 +13,35 @@
     </template>
     <template v-else>
       <ul class="bg-black text-white">
-        <li v-for="(token, index) in tokens" :key="token.tokenId + '_' + index" class="flex flex-wrap md:flex-nowrap min-h-9 justify-between" :class="{'opacity-30': isCaptured(index)}">
+        <li v-for="(token, index) in tokens" :key="token.tokenId + '_' + index" class="flex flex-wrap md:flex-nowrap min-h-9 justify-between" :class="{'opacity-[.45]': !isCaptured(index)}">
           <!-- token link -->
           <div v-if="demoAmount">{{ index }}</div>
-          <a :href="store.getters.openSeaLink({tokenId: token.tokenId})" target="_blank" rel="noopener noreferrer" class="text-neutral-500 order-1 pl-2.5 pr-1.5 py-2.5 block">
+          <a :href="store.getters.openSeaLink({tokenId: token.tokenId})" target="_blank" rel="noopener noreferrer" class="text-neutral-500ff order-1 pl-2.5 pr-1.5 py-2.5 block">
             #{{ token.index }}
           </a>
 
-          <a :href="store.getters.openSeaLink({tokenId: token.tokenId})" target="_blank" rel="noopener noreferrer" class="order-3 px-2.5 pb-2.5 md:py-2.5 md:flex-1 text-left mouse:hover:bg-[rgba(255,255,255,0.1)] flex gap-3">
-            <div class="whitespace-nowrap">
-              shaDoAW of
-            </div>
-            <div>
-              <span v-for="word in mneuomonic(token.tokenId).split(' ')" :key="word" class="uppercase inline-block pr-3" :style="{'color': stringToHexColor(word)}">
-                {{ word }}
-              </span>
+          <a :href="store.getters.openSeaLink({tokenId: token.tokenId})" target="_blank" rel="noopener noreferrer" class="order-3 px-2.5 pb-2.5 md:py-2.5 md:flex-1 text-left mouse:hover:bg-[rgba(255,255,255,0.1)]">
+            <div class="flex gap-3">
+              <div class="whitespace-nowrap">
+                shaDoAW of
+              </div>
+              <div>
+                <span v-for="word in mneuomonic(token.tokenId).split(' ')" :key="word" class="uppercase inline-block pr-3" :style="{'color': stringToHexColor(word)}">
+                  {{ word }}
+                </span>
+              </div>
             </div>
           </a>
           <!-- profile link -->
           <!-- <a :href="$store.getters.openSeaLink({account: token.owner})" target="_blank" rel="noopener noreferrer" class="block pl-3 pr-2.5 h-10 items-center underline mouse:hover:bg-[rgba(255,255,255,0.1)]">
             <Addr :address="token.owner" />
           </a> -->
-          <a :href="store.getters.etherscanLink({tokenId: token.tokenId})" target="_blank" rel="noopener noreferrer" class="order-2 md:order-last flex-1 md:flex-none pl-3 pr-2.5 py-2.5 flex min-w-0 md:min-h-9 items-start justify-end text-right mouse:hover:bg-[rgba(255,255,255,0.1)]">
+          <a :href="isCaptured(index) ? store.getters.openSeaLink({account: token.owner}) : store.getters.etherscanLink({address: token.owner}) + '#nfttransfers'" target="_blank" rel="noopener noreferrer" class="order-2 md:order-last flex-1 md:flex-none pl-3 pr-2.5 py-2.5 flex min-w-0 md:min-h-9 items-start justify-end text-right mouse:hover:bg-[rgba(255,255,255,0.1)]">
             <template v-if="isCaptured(index)">
-              <div>CAPTURED BY <Addr :address="token.owner" class="underline" /></div>
+              <Addr :address="token.owner" class="underline" />
             </template>
-            <span v-else class="underline">
-              WAITING
+            <span v-else class="opacity-30ff">
+              {{store.getters.addrShort(token.owner)}}
             </span>
           </a>
         </li>
